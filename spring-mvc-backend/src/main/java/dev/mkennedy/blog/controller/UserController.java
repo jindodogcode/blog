31c183cb2ec7359c1d1b87ec6a5a844cdf781b6c
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-05 23:34:22 -0400

    added paths

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/UserController.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/UserController.java
@@ -5,8 +5,11 @@ import dev.mkennedy.blog.entity.UserSecurity;
 import dev.mkennedy.blog.entity.UserSecurity.Role;
 import dev.mkennedy.blog.repository.UserRepository;
 import dev.mkennedy.blog.service.UserTransactionService;
+import javax.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +36,18 @@ public class UserController {
         User saved = userService.saveUserAndSecurity(user, security);
 
         return saved;
+    }
+
+    @GetMapping
+    public Iterable<User> searchUsers() {
+        return userRepo.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable("username") String username) {
+        User user = userRepo.findByUsername(username).orElseThrow(() -> new EntityNotFoundException());
+
+        return user;
     }
 }
 
