18bf57c1fdb9e288d9027075edf7aaba16bfe593
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-06 11:57:32 -0400

    added request mapping

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/HomeController.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/HomeController.java
@@ -3,13 +3,21 @@ package dev.mkennedy.blog.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import dev.mkennedy.blog.entity.User;
 
 @Controller
+@RequestMapping("/api/v1")
 public class HomeController {
 
-    @GetMapping("/api/v1/login")
+    @GetMapping("/login")
     public User login(@AuthenticationPrincipal User user) {
+        if (user == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(user);
+        }
+
         return user;
     }
 }
