b2414fa97def85735db9f85e2e3065aedf156257
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-07 13:07:42 -0400

    POST /{username}/posts path

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/UserController.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/UserController.java
@@ -2,14 +2,15 @@ package dev.mkennedy.blog.controller;
 
 import dev.mkennedy.blog.entity.Post;
 import dev.mkennedy.blog.entity.User;
-import dev.mkennedy.blog.entity.UserSecurity;
-import dev.mkennedy.blog.entity.UserSecurity.Role;
+import dev.mkennedy.blog.model.NewPostForm;
+import dev.mkennedy.blog.model.NewUserForm;
 import dev.mkennedy.blog.repository.PostRepository;
 import dev.mkennedy.blog.repository.UserRepository;
 import dev.mkennedy.blog.service.UserTransactionService;
 import javax.persistence.EntityNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,26 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
-    private final UserRepository userRepo;
-    private final UserTransactionService userService;
-    private final PostRepository postRepo;
-
-    public UserController(
-            UserRepository userRepo,
-            UserTransactionService userService,
-            PostRepository postRepo) {
-        this.userRepo = userRepo;
-        this.userService = userService;
-        this.postRepo = postRepo;
-    }
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private UserTransactionService userService;
+    @Autowired
+    private PostRepository postRepo;
 
     @PostMapping
-    public User addUser(@RequestBody User user) {
-        UserSecurity security = user.getSecurity();
-        security.setRole(Role.ROLE_USER);
-        User saved = userService.saveUserAndSecurity(user, security);
+    public User addUser(@Valid @RequestBody NewUserForm userForm) {
+        User saved = userService.saveNewUser(userForm);
 
         return saved;
     }
@@ -52,13 +43,26 @@ public class UserController {
 
     @GetMapping("/{username}")
     public User getUserByUsername(@PathVariable("username") String username) {
-        User user = userRepo.findByUsername(username).orElseThrow(() -> new EntityNotFoundException());
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException());
 
         return user;
     }
 
+    @PostMapping("/{username}/posts")
+    public Post addUserPost(@PathVariable("username") String username,
+            @Valid @RequestBody NewPostForm postForm) {
+        User user = userRepo.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(username + "not found"));
+        Post post = postForm.toPost(user);
+        Post saved = postRepo.save(post);
+
+        return saved;
+    }
+
     @GetMapping("/{username}/posts")
-    public Iterable<Post> getUserPosts(@PathVariable("username") String username) {
+    public Iterable<Post> getUserPosts(
+            @PathVariable("username") String username) {
         return postRepo.findAllByUsername(username);
     }
 }
