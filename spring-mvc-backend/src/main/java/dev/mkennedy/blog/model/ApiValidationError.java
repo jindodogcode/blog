ab72ca6c6eaf1c800c210b5cc5cb8709e7f0db86
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-12 21:20:52 -0400

    Added default constructor

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/model/ApiValidationError.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/model/ApiValidationError.java
@@ -1,102 +1,107 @@
 package dev.mkennedy.blog.model;
 
 public class ApiValidationError extends ApiSubError {
-    
+
     private String object;
     private String field;
     private Object rejectedValue;
     private String message;
+
+    public ApiValidationError() {
+    }
 
     public ApiValidationError(String object, String message) {
         this.object = object;
         this.message = message;
     }
 
-    public ApiValidationError(String object, String field, Object rejectedValue, String message) {
+    public ApiValidationError(String object, String field, Object rejectedValue,
+            String message) {
         this(object, message);
         this.field = field;
         this.rejectedValue = rejectedValue;
     }
 
     public String getObject() {
-      return object;
+        return object;
     }
 
     public void setObject(String object) {
-      this.object = object;
+        this.object = object;
     }
 
     public String getField() {
-      return field;
+        return field;
     }
 
     public void setField(String field) {
-      this.field = field;
+        this.field = field;
     }
 
     public Object getRejectedValue() {
-      return rejectedValue;
+        return rejectedValue;
     }
 
     public void setRejectedValue(Object rejectedValue) {
-      this.rejectedValue = rejectedValue;
+        this.rejectedValue = rejectedValue;
     }
 
     public String getMessage() {
-      return message;
+        return message;
     }
 
     public void setMessage(String message) {
-      this.message = message;
+        this.message = message;
     }
 
     @Override
     public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((field == null) ? 0 : field.hashCode());
-      result = prime * result + ((message == null) ? 0 : message.hashCode());
-      result = prime * result + ((object == null) ? 0 : object.hashCode());
-      result = prime * result
-          + ((rejectedValue == null) ? 0 : rejectedValue.hashCode());
-      return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((field == null) ? 0 : field.hashCode());
+        result = prime * result + ((message == null) ? 0 : message.hashCode());
+        result = prime * result + ((object == null) ? 0 : object.hashCode());
+        result = prime * result
+                + ((rejectedValue == null) ? 0 : rejectedValue.hashCode());
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-      if (this == obj)
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ApiValidationError other = (ApiValidationError) obj;
+        if (field == null) {
+            if (other.field != null)
+                return false;
+        } else if (!field.equals(other.field))
+            return false;
+        if (message == null) {
+            if (other.message != null)
+                return false;
+        } else if (!message.equals(other.message))
+            return false;
+        if (object == null) {
+            if (other.object != null)
+                return false;
+        } else if (!object.equals(other.object))
+            return false;
+        if (rejectedValue == null) {
+            if (other.rejectedValue != null)
+                return false;
+        } else if (!rejectedValue.equals(other.rejectedValue))
+            return false;
         return true;
-      if (obj == null)
-        return false;
-      if (getClass() != obj.getClass())
-        return false;
-      ApiValidationError other = (ApiValidationError) obj;
-      if (field == null) {
-        if (other.field != null)
-          return false;
-      } else if (!field.equals(other.field))
-        return false;
-      if (message == null) {
-        if (other.message != null)
-          return false;
-      } else if (!message.equals(other.message))
-        return false;
-      if (object == null) {
-        if (other.object != null)
-          return false;
-      } else if (!object.equals(other.object))
-        return false;
-      if (rejectedValue == null) {
-        if (other.rejectedValue != null)
-          return false;
-      } else if (!rejectedValue.equals(other.rejectedValue))
-        return false;
-      return true;
     }
 
     @Override
     public String toString() {
-      return "ApiValidationError [field=" + field + ", message=" + message
-          + ", object=" + object + ", rejectedValue=" + rejectedValue + "]";
+        return "ApiValidationError [field=" + field + ", message=" + message
+                + ", object=" + object + ", rejectedValue=" + rejectedValue
+                + "]";
     }
 }
