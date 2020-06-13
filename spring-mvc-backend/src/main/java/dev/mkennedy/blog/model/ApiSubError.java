06adec9120f676703e39d8934e93b602f5176f28
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-12 21:21:45 -0400

    Added JSON serialization type information

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/model/ApiSubError.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/model/ApiSubError.java
@@ -1,5 +1,11 @@
 package dev.mkennedy.blog.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ @Type(value = ApiValidationError.class, name = "validation error") })
 public abstract class ApiSubError {
 
 }
