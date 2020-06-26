95ee1ca0d447d8531d6cc2af0d8878950e79072e
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-06-26 10:11:58 -0400

    Added me route

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/HomeController.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/HomeController.java
@@ -2,10 +2,13 @@ package dev.mkennedy.blog.controller;
 
 import java.time.ZonedDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import dev.mkennedy.blog.entity.User;
 import dev.mkennedy.blog.repository.UserRepository;
 
@@ -17,11 +20,34 @@ public class HomeController {
 
     @GetMapping("/api/v1/login")
     public User login(Authentication authentication) {
+        // System.out.println(authentication);
+        // if (authentication == null) {
+        //     throw new UnauthorizedException();
+        // }
         User user = userRepo.findByUsername(authentication.getName())
             .orElseThrow(() -> new UsernameNotFoundException(
                 "username: " + authentication.getName() + " not found"));
         user.setLastLoggedIn(ZonedDateTime.now());
 
         return userRepo.save(user);
+    }
+
+    @GetMapping("/api/v1/me")
+    public User me(Authentication authentication) {
+        System.out.println(authentication);
+        if (authentication == null) {
+            throw new UnauthorizedException();
+        }
+        User user = userRepo.findByUsername(authentication.getName())
+            .orElseThrow(() -> new UsernameNotFoundException(
+                "username: " + authentication.getName() + " not found"));
+        user.setLastLoggedIn(ZonedDateTime.now());
+
+        return userRepo.save(user);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public class UnauthorizedException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
     }
 }
