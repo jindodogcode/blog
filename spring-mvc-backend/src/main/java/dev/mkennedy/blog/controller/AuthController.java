59d4e8385b21384546a8fea120c6d7cee7fe6d40
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-07-30 10:40:59 -0400

    Rename HomeController to AuthController

--- spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/AuthController.java
+++ spring-mvc-backend/src/main/java/dev/mkennedy/blog/controller/AuthController.java
@@ -1,11 +1,13 @@
 package dev.mkennedy.blog.controller;
 
 import java.time.ZonedDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,13 @@ import dev.mkennedy.blog.entity.User;
 import dev.mkennedy.blog.repository.UserRepository;
 
 @RestController
-public class HomeController {
+@RequestMapping("/api/v1")
+public class AuthController {
 
     @Autowired
     private UserRepository userRepo;
 
-    @GetMapping("/api/v1/login")
+    @GetMapping("/login")
     public User login(Authentication authentication) {
         User user = userRepo.findByUsername(authentication.getName())
             .orElseThrow(() -> new UsernameNotFoundException(
@@ -28,7 +31,7 @@ public class HomeController {
         return userRepo.save(user);
     }
 
-    @GetMapping("/api/v1/me")
+    @GetMapping("/me")
     public User me(Authentication authentication) {
         System.out.println(authentication);
         if (authentication == null) {
